cf94b054a17f18e014cf2a3451ada17e31943ff7
Kristian Kanya <kriskanya@gmail.com>
2014-08-27 16:06:32 -0500

    added specs for viewing plant varieties, pending implementation

---
+++ spec/features/user_views_plant_varieties_spec.rb
@@ -0,0 +1,55 @@
+# As a user
+# I want to see a list of all of the plant categories and their corresponding varieties
+# So that I can select a specific plant to add to the garden
+#
+# Acceptance Criteria
+#
+# User must be logged in and on the garden edit page
+# All plant categories must appear in the drop down
+# Once a category is selected, the name of the category appears in the drop down box
+# If no plant categories exist, No plants available should appear in the drop down box
+# If a selected category has no plant varieties exist, No plant varieties should appear in the drop down box
+# All varieties must appear in the drop down list after a category is selected
+# Usage
+#
+# From the garden edit page
+# User clicks on the Plants tab
+# User clicks on the Add drop/down
+# The drop down expands to show the listing of all plant categories
+# The user clicks on a plant category
+# The drop down list shows all of the plant varieties for the selected category
+
+feature "User views plant varieties" do
+  background do
+    @user = Fabricate(:user)
+    @garden = Fabricate(:garden)
+    login_as @user
+    visit edit_garden_path(@garden)
+    click_on "Plants"
+    click_on "Add"
+  end
+
+  scenario "Happy path - user finds what they're looking for" do
+    pending "Further implementation"
+    @tomatoes = Fabricate(:category, name: "Tomatoes", edible: true)
+    Fabricate(:category, name: "Squash", edible: true)
+    Fabricate(:variety, name: "Cherry", description: "some description", category: @tomatoes)
+    Fabricate(:variety, name: "Grape", description: "some description", category: @tomatoes)
+    page.should have_content("Tomatoes")
+    page.should have_content("Squash")
+    click_on "Tomatoes"
+    page.should have_content("Cherry")
+    page.should have_content("Grape")
+  end
+
+  scenario "No categories exist" do
+    pending "Further implementation"
+    page.should have_content("No plants available")
+  end
+
+  scenario "No variety for selected category" do
+    pending "Further implementation"
+    click_on "Tomatoes"
+    page.should have_content("No plant varieties")
+  end
+end
