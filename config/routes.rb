e5019d27690dd9676a8ffc9f32954058e573950d
Rachel Self <rachel.m.self@gmail.com>
2014-08-28 10:25:55 -0500

    Finish testing and implementation for viewing plant varieties

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
 
   resources :categories, only: [:index, :new, :create, :show] do
-    resources :varieties, only: [:new, :create]
+    resources :varieties, only: [:new, :create, :show]
   end
   resources :gardens, only: [:index, :create, :edit]
   resource :user_session, only: [:new, :create, :destroy]
