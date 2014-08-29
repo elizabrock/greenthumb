215d62e3f61f44bb89176f4e19369485d1616bce
CarterNelms <carternelms@bellsouth.net>
2014-08-29 11:37:00 -0500

    User reset password feature complete

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -16,10 +16,10 @@ class PasswordResetsController < ApplicationController
       if @user.deliver_reset_password_instructions!
         redirect_to(root_path, :notice => 'Instructions have been sent to your email.')
       else
-        redirect_to(root_path, :notice => 'Email was not successfully sent.')
+        redirect_to(root_path, :alert => 'Email was not successfully sent.')
       end
     else
-      redirect_to(root_path, :notice => "Couldn't find user.")
+      redirect_to(root_path, :alert => "Couldn't find user.")
     end
     # Tell the user instructions have been sent whether or not email was found.
     # This is to not leak information to attackers about which emails exist in the system.
@@ -39,8 +39,8 @@ class PasswordResetsController < ApplicationController
 
   # This action fires when the user has sent the reset password form.
   def update
-    @token = params[:token]
-    @user = User.load_from_reset_password_token(params[:token])
+    @token = params[:user][:token]
+    @user = User.load_from_reset_password_token(@token)
 
     if @user.blank?
       not_authenticated
@@ -48,11 +48,18 @@ class PasswordResetsController < ApplicationController
     end
 
     # the next line makes the password confirmation validation work
-    @user.password_confirmation = params[:user][:password_confirmation]
+    # @user.password_confirmation = params[:user][:password_confirmation]
+    is_password_confirmed = params[:user][:password_confirmation] == params[:user][:new_password]
     # the next line clears the temporary token and updates the password
-    if @user.change_password!(params[:user][:password])
-      redirect_to(root_path, :notice => 'Password was successfully updated.')
+    if is_password_confirmed
+      if @user.change_password!(params[:user][:new_password])
+        redirect_to(root_path, :notice => 'Password was successfully updated.')
+      else
+        flash[:alert] = "Could not reset password."
+        render :action => "edit"
+      end
     else
+      flash[:alert] = "Passwords do not match."
       render :action => "edit"
     end
   end
--- spec/features/user_resets_password_spec.rb
+++ spec/features/user_resets_password_spec.rb
@@ -1,24 +1,52 @@
-RSpec.describe UserMailer do
-  # background do
-  #   @user = Fabricate(:user)
-  #   login_as @user
-  # end
+feature "User resets password" do
+  scenario 'Happy path, reset password and log in' do
+    @user = Fabricate(:user)
+    visit '/'
+    click_link 'Sign In'
+    click_link 'Reset your password'
+    fill_in "Email", with: @user.email
+    click_on "Send Email"
+    @user = User.find_by_email(@user.email)
+    visit "/password_resets/#{@user.reset_password_token}/edit"
+    fill_in "New password", with: 'abc123'
+    fill_in "Password confirmation", with: 'abc123'
+    click_on "Submit"
 
-  describe 'resetting password' do
-    it "sends an email to reset password when not logged in" do
-      pending "further implementation"
-      @user = Fabricate(:user)
-      visit '/'
-      click_link 'Sign In'
-      click_link 'Reset your password'
-      fill_in "Email", with: @user.email
-      click_on "Send Email"
-      puts "/password_resets/#{@user.reset_password_token}/edit"
-      visit "/password_resets/#{@user.reset_password_token}/edit"
-      fill_in "Email", with: @user.email
-      fill_in "Password", with: 'abc123'
-      fill_in "PasswordConfirmation", with: 'abc123'
-      click_on "Submit"
-    end
+    current_path.should == root_path
+    page.should have_content("Password was successfully updated.")
+    click_link "Sign In"
+    fill_in "Email", with: @user.email
+    fill_in "Password", with: "abc123"
+    click_button "Sign In"
+    page.should have_content("Welcome back, #{@user.email}")
+    current_path.should == gardens_path
+  end
+
+  scenario 'Sad path, email is not regisered in the database' do
+    @user = Fabricate(:user)
+    visit '/'
+    click_link 'Sign In'
+    click_link 'Reset your password'
+    fill_in "Email", with: "abc" + @user.email
+    click_on "Send Email"
+    current_path.should == root_path
+    page.should have_content("Couldn't find user.")
+  end
+
+  scenario 'Sad path, password confirmation does not match new password' do
+    @user = Fabricate(:user)
+    visit '/'
+    click_link 'Sign In'
+    click_link 'Reset your password'
+    fill_in "Email", with: @user.email
+    click_on "Send Email"
+    @user = User.find_by_email(@user.email)
+    visit "/password_resets/#{@user.reset_password_token}/edit"
+    fill_in "New password", with: 'abc123'
+    fill_in "Password confirmation", with: 'bbc123'
+    click_on "Submit"
+
+    current_path.should == "/password_resets/#{@user.reset_password_token}"
+    page.should have_content("Passwords do not match.")
   end
 end
