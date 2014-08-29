c616a45cdee24dd68aed79e278a2d5964f115e49
CarterNelms <carternelms@bellsouth.net>
2014-08-29 11:59:49 -0500

    removed hammering protection, added user email to email text

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -333,7 +333,7 @@ Rails.application.config.sorcery.configure do |config|
     # hammering protection, how long to wait before allowing another email to be sent.
     # Default: `5 * 60`
     #
-    # user.reset_password_time_between_emails = 10
+    user.reset_password_time_between_emails = 0
 
 
     # -- brute_force_protection --
