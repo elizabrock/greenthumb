35a68dc8bfed3ad0618092c80a6fae98ca78cc8a
wesmangum <wesmangum24@gmail.com>
2014-09-05 14:47:48 -0500

    Removed commented JS from `garden_edit.js`

--- app/assets/javascripts/garden_edit.js
+++ app/assets/javascripts/garden_edit.js
@@ -13,18 +13,7 @@
         type: "GET",
         dataType: "html",
         success: function(html){
-            $('#varieties_list').replaceWith(html);
-            // if(varieties.length) {
-            //   $('#varieties_list').empty();
-            //   varieties.forEach (function(variety){
-            //     var variety_link = $("<a>");
-            //     $(variety_link).attr("href", "#").append(variety.name + " (" + variety.description +")");
-            //     $('#varieties_list').append(variety_link);
-            //   });
-            // }
-            // else {
-            //   $('#varieties_list').append("<p>No varieties exist</p>")
-            // }
+          $('#varieties_list').replaceWith(html);
         }
       });
     });
